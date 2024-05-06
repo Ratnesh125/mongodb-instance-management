3befea98d15b6ac21b104b06044916d93bf465b7
Ratnesh Bharti <87529730+Ratnesh125@users.noreply.github.com>
2024-05-06 17:58:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mongodb-instance-management/"
+    plugins: [react()],
+    base: "/mongodb-instance-management/"
 })
